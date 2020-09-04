83b583616e57f71dcca48e0ebfe080c2ea6ed8d5
xzha0203 <xzha0203@student.monash.edu>
2020-09-04 12:24:05 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,35 @@
+import warnings
+warnings.simplefilter('ignore')
+import pandas as pd
+import pymysql
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegressionCV
+import joblib
+from sqlalchemy import create_engine
+
+
+host="heartstationdatabase.cvkcfi0mgtaa.us-east-1.rds.amazonaws.com"
+port=int(3306)
+dbname="heart_data"
+user="heartstation"
+password="PG5cBGTGgqrMv4Dz1lYu"
+
+engine = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}'.format(user=user, password=password, host=host, port=port, dbname=dbname))
+
+heart_df = pd.read_sql_table('heart_df', engine)
+
+x=heart_df.iloc[:,:-1]
+y=heart_df.iloc[:,-1]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, random_state=5)
+
+lr = LogisticRegressionCV(cv=5, random_state=0).fit(x_train, y_train)
+
+joblib.dump(lr, 'model.pkl')
+print("Model dumped!")
+
+model_columns = list(x.columns)
+
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
